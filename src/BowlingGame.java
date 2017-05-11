330b6728e4330a1c77feada1f3201e85adc48cb8
somename <someemail>
2017-05-11 20:21:05 +0200

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -24,7 +24,7 @@ public class BowlingGame {
 	
 	// Sets the bonus throws at the end of the game
 	public void setBonus(int firstThrow, int secondThrow) {
-		bonus=
+		
 	}
 	
 	// Returns the game score
--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -12,10 +12,10 @@ public class TestBowling {
 	@Test
 	public void test_isStrike() throws BowlingException {
 		
-		BowlingGame game = new BowlingGame();
+		//BowlingGame game = new BowlingGame();
 		Frame frame = new Frame(10,0);
 		
-		assertEquals("Incorrect when firstThrow is 10 and secondThrow is 0", frame.isStrike());
+		assertTrue("It is strike", frame.isStrike());
 		
 	}
 
